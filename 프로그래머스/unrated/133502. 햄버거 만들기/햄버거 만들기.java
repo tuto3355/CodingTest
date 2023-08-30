1cd088ffed2a6d858b51541d350bcf99da7a3783
tuto3355 <130144714+tuto3355@users.noreply.github.com>
2023-08-30 17:53:59 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.04 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        int[] arr = new int[]{1, 2, 3, 1};
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int num = 0;
+
+        while (true) {
+            while (list.size()>=4) {
+                int cnt = 0;
+                for (int i = 0; i < 4; i++) {
+                    if (!(arr[i] == list.get(list.size() - 4 + i))) {
+                        cnt++;
+                        break;
+                    }
+                }
+                if (cnt == 0) {
+                    for (int i = 0; i < 4; i++) {
+                        list.remove(list.size() - 1);
+                    }
+                    answer++;
+                } else {
+                    break;
+                }
+            }
+            if (num >= ingredient.length) {
+                break;
+            }
+            if(num< ingredient.length)
+                list.add(ingredient[num]);
+            num++;
+            
+        }
+
+        return answer;
+    }
+}
